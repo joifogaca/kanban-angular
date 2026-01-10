efe64ca54f548ffdbea6ef4e6cc2c8be4a1de930
Joice Fogaça <joi.fogaca@gmail.com>
2026-01-10 11:44:45 -0300

    criação estruturas colunas kanban

--- src/app/quadro-kanban/coluna-kanban/coluna-kanban.component.ts
+++ src/app/quadro-kanban/coluna-kanban/coluna-kanban.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-coluna-kanban',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './coluna-kanban.component.scss'
 })
 export class ColunaKanbanComponent {
+
+  @Input({ required: true }) nomeColuna!: string;
 
 }
--- src/app/quadro-kanban/quadro-kanban.component.ts
+++ src/app/quadro-kanban/quadro-kanban.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ColunaKanbanComponent } from "./coluna-kanban/coluna-kanban.component";
 
 @Component({
   selector: 'app-quadro-kanban',
   standalone: true,
-  imports: [],
+  imports: [ColunaKanbanComponent],
   templateUrl: './quadro-kanban.component.html',
   styleUrl: './quadro-kanban.component.scss'
 })
