2eba05839539464eed7beed5a04ecbb9c90240ae
Joice Fogaça <joi.fogaca@gmail.com>
2026-01-10 11:23:14 -0300

    pagina-inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./quadro-kanban/quadro-kanban.component').then(m => m.QuadroKanbanComponent)
+  }
+];
