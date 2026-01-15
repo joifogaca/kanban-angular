39c2d4b3c658e7ca708796c87a8b0a6126db849c
Joice Fogaça <joi.fogaca@gmail.com>
2026-01-15 17:05:56 -0300

    arrastar sobre a coluna

--- src/app/kanban-page/coluna-kanban/coluna-kanban.component.ts
+++ src/app/kanban-page/coluna-kanban/coluna-kanban.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 
-import { NgFor } from '@angular/common';
+import { NgFor, NgClass } from '@angular/common';
 import { CartaoKanbanComponent } from "../cartao-kanban/cartao-kanban.component";
 import { Card } from '../shared/card.model';
 
 @Component({
   selector: 'app-coluna-kanban',
   standalone: true,
-  imports: [CartaoKanbanComponent, NgFor],
+  imports: [CartaoKanbanComponent, NgFor, NgClass],
   templateUrl: './coluna-kanban.component.html',
   styleUrl: './coluna-kanban.component.scss',
   host: {
@@ -19,5 +19,14 @@ export class ColunaKanbanComponent {
   @Input({ required: true }) afterColor: string = '#d573b6';
   @Input({ required: true }) nomeColuna!: string;
   @Input({ required: true }) cards!: Card[];
+
+  card_over: boolean = false;
+  dragover(){
+    this.card_over = true;
+  }
+
+  dragleave(){
+    this.card_over = false;
+  }
 
 }
